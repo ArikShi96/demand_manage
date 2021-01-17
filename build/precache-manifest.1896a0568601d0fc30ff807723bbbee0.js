6ff80f76ec97c9fcafdfcb847ec876c2e92fafc0
liuchanchan <m13120282633@163.com>
2021-01-17 19:30:09 +0800

    'bug'

---
+++ build/precache-manifest.1896a0568601d0fc30ff807723bbbee0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b784157a283cd937e48d0a4c99e06538",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a47d146ca985bf6aaad4",
+    "url": "./static/css/2.0ccbae64.chunk.css"
+  },
+  {
+    "revision": "52ee26e7886c83fa2f9a",
+    "url": "./static/css/main.c847f65b.chunk.css"
+  },
+  {
+    "revision": "a47d146ca985bf6aaad4",
+    "url": "./static/js/2.19bb49de.chunk.js"
+  },
+  {
+    "revision": "188833454a12b3f4d5eed08c78befc31",
+    "url": "./static/js/2.19bb49de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ee26e7886c83fa2f9a",
+    "url": "./static/js/main.107fb342.chunk.js"
+  },
+  {
+    "revision": "0e724e6281f2adb136df",
+    "url": "./static/js/runtime-main.a23952db.js"
+  },
+  {
+    "revision": "d9f393cd00d70def4413e758a4cdfdd4",
+    "url": "./static/media/date.d9f393cd.svg"
+  },
+  {
+    "revision": "3e130aeae3c876dce1224c8ed546f0de",
+    "url": "./static/media/iconfont.3e130aea.svg"
+  },
+  {
+    "revision": "419060b0df7ced3eaa85985666aac0c3",
+    "url": "./static/media/iconfont.419060b0.woff"
+  },
+  {
+    "revision": "a9af776046d1032f6c7f9f108e007ea6",
+    "url": "./static/media/iconfont.a9af7760.eot"
+  },
+  {
+    "revision": "ba957f58da8920f08382bc81a8c412c5",
+    "url": "./static/media/iconfont.ba957f58.ttf"
+  },
+  {
+    "revision": "069381f0c706c19be9a4bb4b9592b3bd",
+    "url": "./static/media/iuap-logo.069381f0.svg"
+  },
+  {
+    "revision": "3749f606ccdcf2bb964b42247a7bdc0a",
+    "url": "./static/media/product.3749f606.svg"
+  }
+]);
